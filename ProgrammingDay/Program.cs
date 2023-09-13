a9a15a4a6b4046fbfe36c6ac9667fb5145a6600d
ho.ghandehari <ho.ghandehari@behsacorp>
2023-09-13 14:00:12 +0330

    Add project files.

---
+++ ProgrammingDay/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProgrammingDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input = string.Empty;
+            do
+            {
+
+                Console.WriteLine("enter input (-999 to exit):");
+
+                input = Console.ReadLine();
+
+
+
+
+            }while(input != "-999");
+
+        }
+    }
+}
